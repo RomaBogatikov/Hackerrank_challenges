94c3b42fb09f170cf0e12eb3109b9929ff49221c
RomanBogatikov <ivaroman2000@gmail.com>
2019-06-22 15:56:30 -0400

    solution to 'Hash Tables: Ransom Note'

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains my solutions to problems from Hackerrank: Interview Preparation Kit.
---
+++ ransom_note.js
@@ -0,0 +1,53 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the checkMagazine function below.
+function checkMagazine(magazine, note) {
+    if (note.every((element, index) => {
+        // if an element from note is in the magazine
+        if (magazine.includes(element)) {
+            // splice this element from magazine array
+            magazine.splice(magazine.indexOf(element), 1, null);
+            return true;
+        }
+    })) {
+        console.log('Yes');
+    } else {
+        console.log('No');
+    }
+}
+
+function main() {
+    const mn = readLine().split(' ');
+
+    const m = parseInt(mn[0], 10);
+
+    const n = parseInt(mn[1], 10);
+
+    const magazine = readLine().split(' ');
+
+    const note = readLine().split(' ');
+
+    checkMagazine(magazine, note);
+}
