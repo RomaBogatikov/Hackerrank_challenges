4f3885777bbdc28323e533fbefbfa118d3c2b814
RomanBogatikov <ivaroman2000@gmail.com>
2019-06-23 08:38:46 -0400

    solved String Manipulation: Alternating Characters

---
+++ alternating_characters.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the alternatingCharacters function below.
+function alternatingCharacters(s) {
+    // iterate over every char in string s
+    // if char at position i === to char at postion i+1
+    // char at position i needs to be removed (counter increased by 1)
+    let counter = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        (s[i] === s[i + 1]) && ++count;
+    }
+    return counter;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        let result = alternatingCharacters(s);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
